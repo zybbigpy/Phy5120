f70b6137ad2bfa5a1d692839f4acbb968c6b2377
zybbigpy <miaowangqian@icloud.com>
2019-09-22 11:37:59 +0800

    most of hw1

--- hw1/report/hw1_report.md
+++ hw1/report/hw1_report.md
@@ -1,26 +1,53 @@
 # PHYS ​5120: Homework 1
 
-> Author: MIAO Wangqian, Student ID: 20617902
+Author: MIAO Wangqian, Student ID: 20617902
 
-## 1. The Linear and nonlinear pendulums
+## 1. How to build my code
 
-### 1.1 
+First of all, clone my code from Github.
+
+```bash
+git clone git@github.com:zybbigpy/Phy5120.git
+```
+
+Then, use `git pull` to get the latest code and report.
+
+```bash
+git pull origin master
+```
+
+If your system support `Make`, just use the command below. if you have `pandoc` and LaTeX in your system, you can build the pdf file. Otherwise you just enter the report directory to get the pdf file.
+
+```bash
+# go to hw1
+cd hw1
+# run code
+make run
+# make pdf file
+make doc
+# clean data
+make clean
+```
+
+## 2. The Linear and nonlinear pendulums
+
+### 2.1 Solution
 
 The equation of motion is in the format of:
 
-$$ \frac{\mathrm{d^2 \theta}}{\mathrm{d} t^2} + \frac{g}{\ell}\theta = 0$$.
+$$ \frac{\mathrm{d^2}\theta}{\mathrm{d} t^2} + \frac{g}{\ell}\theta = 0$$
 
 And the solution of the differential equation is:
 
-$$\theta =  A \cos (\sqrt\frac{g}{\ell} t + \delta)$$.
+$$\theta =  A \cos (\sqrt\frac{g}{\ell} t + \delta)$$
 
 There are two parameters $A, \delta$ in the solution because we do not know the initial condition $\theta(t=0), \dot{\theta}(t=0)$.
 
 The swing period is:
 
-$$T = 2\pi \sqrt\frac{\ell}{g}$$.
+$$T = 2\pi \sqrt\frac{\ell}{g}$$
 
-### 1.2
+### 2.2 Solution
 
 
 
--- hw1/src/pendulum.py
+++ hw1/src/pendulum.py
@@ -1,29 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import os
+
 """
 The equation we want to solve:
 \theta(t+h)=2\theta(t)-\theta(t-h)+(-g/l)\theta(t)h^2
 \theta(t=0)=1/30
 """
 
-# constant
-g = 9.8
-L = 0.1
-h = 0.01
-m = 1
 
-# initial condition
-theta_0 = float(1 / 30)
+def mkdir(path):
+
+    folder = os.path.exists(path)
+
+    if not folder:
+        os.makedirs(path)
+        print("create new folder")
+    else:
+        print("folder exists")
 
 
-def theta_generator(steps, small_angle=True):
+def theta_generator(theta_0, h, time_length):
     # initial condition
     i, theta_t_minus_h, theta_t = 0, theta_0, theta_0
-    while i < steps:
+    while i < time_length:
         yield theta_t
         # Verlet method
-        if small_angle:
+        if theta_0 < float(10 / 180 * math.pi):
             theta_t_plus_h = 2 * theta_t - theta_t_minus_h - g / L * theta_t * h**2
         else:
             theta_t_plus_h = 2 * theta_t - theta_t_minus_h - g / L * math.sin(theta_t) * h**2
@@ -31,20 +35,46 @@ def theta_generator(steps, small_angle=True):
         i, theta_t, theta_t_minus_h = i + 1, theta_t_plus_h, theta_t
 
 
-# theta list with time
-theta = [x for x in theta_generator(100)]
-time = [t for t in range(100)]
+def compute_theta_energy(theta_0, h, time_length):
+    # theta list with time
+    theta = [x for x in theta_generator(theta_0, h, time_length)]
+    # the unit of time is `h` s
+    time = [t for t in range(time_length)]
 
-# theta + h/2 with time, half represent time at t + h/2
-time_half = [t + h / 2 for t in time]
-time_half.pop(-1)
-theta_half = [(theta[i + 1] - theta[i]) / 2 for i in range(len(theta))]
-velocity_half = [L * (theta[i + 1] - theta[i]) / (2 * h) for i in range(len(theta))]
-T_half = [1 / 2 * m * v**2 for v in velocity_half]
-V_half = [m * g * L * (1 - math.cos(theta)) for theta in theta_half]
-E_half = np.array(T_half) + np.array(V_half)
-assert len(T_half) == len(V_half), "Error"
+    # theta + h/2 with time, half represent time at t + h/2
+    time_half = [t + h / 2 for t in time]
+    time_half.pop(-1)
+    theta_half = [(theta[i + 1] + theta[i]) / 2 for i in range(len(theta) - 1)]
+    velocity_half = [L * (theta[i + 1] - theta[i]) / (2 * h) for i in range(len(theta) - 1)]
+    T_half = [1 / 2 * m * v**2 for v in velocity_half]
+    V_half = [m * g * L * (1 - math.cos(theta)) for theta in theta_half]
+    E_half = np.array(T_half) + np.array(V_half)
+    assert len(T_half) == len(V_half), "Error"
+    return time, theta, time_half, E_half
 
-print(len(theta))
-print(len(time))
-plt.plot(time, theta)
+
+def make_plots(time, theta, time_half, E_half, E_exact):
+    mkdir("./figs")
+    plt.plot(time, theta)
+    plt.savefig("./figs/theta_time.png", dpi=400)
+    plt.clf()
+
+    plt.plot(time_half, E_half)
+    plt.hlines(E_exact, time_half[0], time_half[-1], colors="c", linestyles="dashed")
+    plt.savefig("./figs/energy_time.png", dpi=400)
+
+
+if __name__ == "__main__":
+    # constant
+    g = 9.8
+    L = 0.1
+    h = 0.01
+    m = 1
+    time_length = 100
+
+    # initial condition
+    theta_0 = float(1 / 30)
+    # exact energy
+    E_exact = m * g * L * (1 - math.cos(theta_0))
+    time, theta, time_half, E_half = compute_theta_energy(theta_0, h, time_length)
+    make_plots(time, theta, time_half, E_half, E_exact)
