4e7cda0e586841dd1057415b604dbb68c58e6ae3
zybbigpy <miaowangqian@icloud.com>
2019-10-07 13:09:16 +0800

    for itsc

--- hw2/md.py
+++ hw2/md.py
@@ -8,6 +8,9 @@ Reduced unit: \sigma=\epsilon=mass=k_B=1
 
 import numpy as np
 from itertools import product
+import math
+
+
 rho = 0.6  # density of Argon in reduced units
 dt = 0.004  # time step size
 T_0 = 2  # temperature
@@ -17,6 +20,7 @@ L_box = (N / rho)**(1 / 3.0)  # length of the whole simulation box
 L_cell = L_box / N_cell  # length of a unitcell
 F = np.zeros((N, N, 3))  # matrix that contains all forces
 ind = np.triu_indices(N, k=1)  # indices of upper triangular matrix
+r_final = np.zeros((N,N))
 
 
 def IC_pos(N_cell, L_cell):
@@ -42,14 +46,23 @@ def IC_vel(N):
 def find_force(pos, L_box=L_box):
     '''
     Minimum image convention. 
-    '''
+    '''    
+    print("before asign the F shape")
+    print(F.shape)
     r_vec = pos[ind[0]] - pos[ind[1]]
+    # why here?
     r_vec = r_vec - np.rint(r_vec / L_box) * L_box
     r_sq = np.sum(r_vec**2, axis=1)
     F_vec = -(48 / r_sq**7 - 24 / r_sq**4)[:, None] * r_vec
+    r_final[ind[0], ind[1]] = r_sq**(1/2)
+
     F[ind[0], ind[1]] = F_vec
+    print("in find force, the F shape")
+    print(F.shape)
+    print(id(F))
     pot = np.sum(4 / r_sq**6 - 4 / r_sq**3)
     P = np.sum(F_vec * r_vec)
+    #  why minus instead of plus
     return np.sum(F, axis=0) - np.sum(F, axis=1), pot, P
 
 
@@ -60,30 +73,51 @@ def time_step(pos, vel, F):
     #pos = np.mod(pos + vel * dt, L_box) # why both pos and pos_folded?
     F, pot, P = find_force(pos_folded)
 
+    print("in time step, the F shape is")
+    print(F.shape)
+    print(id(F))
     vel += 0.5 * F * dt
     kin = 0.5 * np.sum(vel**2)
+    # potential and kenetic energy
     return pos, vel, F, pot, kin, P
 
 
 def simulate():
-    kins, pots, Ps = [], [], []
+    kins, pots, Ps, msd_list = [], [], [], []
     pos = IC_pos(N_cell, L_cell)
+    pos_0 = pos
     vel = IC_vel(N)
     F = find_force(pos)[0]
     for t in range(2000):
         pos, vel, F, pot, kin, P = time_step(pos, vel, F)
         if t > 1000:  # production run
+            msd = np.sum((pos-pos_0)**2)/N
+            msd_list.append(msd)
             kins.append(kin)
             pots.append(pot)
             Ps.append(P)
         else:  # equillirum run
             vel *= np.sqrt(N * 3 * T_0 / (2 * kin))
-    return np.array(kins), np.array(pots), np.array(Ps)
+    return np.array(kins), np.array(pots), np.array(Ps), np.array(msd_list)
 
+def radial_distribution(r_final):
+    # calculate the radial distribution of the first atom
+    r_distribution = np.array(r_final[0])
+    r_ = np.linspace(1, np.max(r_distribution), 100)
+    dr = (np.max(r_distribution) - 1)/100
+    v = r_**2*4*math.pi*dr
+    cout = np.zeros(100)
+
+    for r in r_distribution:
+        for i in range(100):
+            if(r>r_[i]):
+                cout[i] +=1
+
+    return np.array(cout)/np.array(v), r_
 
 # The simulation starts here
 if __name__ == "__main__":
-    kins, pots, Ps = simulate()
+    kins, pots, Ps, msds = simulate()
 
     T = np.mean(kins * 2 / (3 * N))  # temperature
     P = 1 - np.mean(Ps) / (3 * N * T) - 16 * np.pi * rho / (
